a7554be5491f9945ff9065b4a6841211aab77642
Hoo Man <hoomanfor@gmail.com>
2020-05-15 21:36:19 -0400

    shopping cart total changes based on quantity change

--- public/javascript/logic.js
+++ public/javascript/logic.js
@@ -3,6 +3,51 @@ $(document).ready(function() {
   // setToken();
 });
 
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+    'use strict';
+  
+    window.addEventListener('load', function() {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName('needs-validation');
+  
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+          const firstName = $("#firstName").val().trim();
+          const lastName = $("#lastName").val().trim();
+          const fullName = firstName + lastName;
+          const address1 = $("#address1").val().trim();
+          if (fullName.length > 30) {
+            document.getElementById("firstName").setCustomValidity('Your combined first/last name cannot be greater than 30 characters.');
+            document.getElementById("lastName").setCustomValidity('Your combined first/last name cannot be greater than 30 characters');
+            $("#firstName-invalid-feedback").text("Your combined first/last name cannot be greater than 30 characters");
+            $("#lastName-invalid-feedback").text("Your combined first/last name cannot be greater than 30 characters");
+          }
+          else {
+            document.getElementById("firstName").setCustomValidity('');
+            document.getElementById("lastName").setCustomValidity('');
+            $("#firstName-invalid-feedback").text("This field is required.");
+            $("#lastName-invalid-feedback").text("This field is required.");
+          }
+          if (address1.length > 30) {
+              document.getElementById("address1").setCustomValidity('Address 1 cannot be greater than 30 characters.');
+              $("#address1-invalid-feedback").text("Address 1 cannot be greater than 30 characters");
+          } else {
+              document.getElementById("address1").setCustomValidity('');
+              $("#address1-invalid-feedback").text("This field is required");
+          }
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          
+          form.classList.add('was-validated');
+        }, false);
+      });
+    }, false);
+})();
+
 function setToken() {
     const now = new Date();
     const unixTimestamp = now.getTime();
@@ -93,53 +138,8 @@ $("#getPrintJobs").on("click", function() {
     })
 });
 
-
-
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(function() {
-  'use strict';
-
-  window.addEventListener('load', function() {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation');
-
-    // Loop over them and prevent submission
-    var validation = Array.prototype.filter.call(forms, function(form) {
-      form.addEventListener('submit', function(event) {
-        const firstName = $("#firstName").val().trim();
-        const lastName = $("#lastName").val().trim();
-        const fullName = firstName + lastName;
-        const address1 = $("#address1").val().trim();
-        if (fullName.length > 30) {
-          document.getElementById("firstName").setCustomValidity('Your combined first/last name cannot be greater than 30 characters.');
-          document.getElementById("lastName").setCustomValidity('Your combined first/last name cannot be greater than 30 characters');
-          $("#firstName-invalid-feedback").text("Your combined first/last name cannot be greater than 30 characters");
-          $("#lastName-invalid-feedback").text("Your combined first/last name cannot be greater than 30 characters");
-        }
-        else {
-          document.getElementById("firstName").setCustomValidity('');
-          document.getElementById("lastName").setCustomValidity('');
-          $("#firstName-invalid-feedback").text("This field is required.");
-          $("#lastName-invalid-feedback").text("This field is required.");
-        }
-        if (address1.length > 30) {
-            document.getElementById("address1").setCustomValidity('Address 1 cannot be greater than 30 characters.');
-            $("#address1-invalid-feedback").text("Address 1 cannot be greater than 30 characters");
-        } else {
-            document.getElementById("address1").setCustomValidity('');
-            $("#address1-invalid-feedback").text("This field is required");
-        }
-        if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-        
-        form.classList.add('was-validated');
-      }, false);
-    });
-  }, false);
-})();
-
-$("[type='submit']").on("click", function() {
-  let firstName = $("#firstName").val().trim();
-});
+$("#quantity").on("change", function() {
+    const quantity = Number($(this).val());
+    let total = quantity * 12;
+    $("#total").html(total);
+})
