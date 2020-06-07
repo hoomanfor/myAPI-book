c70f94e85437eb3cf3f90257803ee94c9a93a911
Hoo Man <hoomanfor@gmail.com>
2020-06-07 15:47:25 -0400

    refactored input validation of shipping address and billing address forms for checkout

--- public/javascript/logic.js
+++ public/javascript/logic.js
@@ -1,175 +1,121 @@
-$(document).ready(function() {
-  console.log("ready!");
-  // setToken();
-});
+$(document).ready(function () {
+    console.log("ready!");
 
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(function() {
-    'use strict';
-  
-    window.addEventListener('load', function() {
-      // Fetch all the forms we want to apply custom Bootstrap validation styles to
-      var forms = document.getElementsByClassName('needs-validation');
-      // Loop over them and prevent submission
-      var validation = Array.prototype.filter.call(forms, function(form) {
-        form.addEventListener('submit', function(event) {
-          const billingAddress = $("#same-address").prop("checked");
-          const firstName = $("#firstName").val().trim();
-          const lastName = $("#lastName").val().trim();
-          const fullName = firstName + lastName;
-          const address1 = $("#address1").val().trim();
-          const baFirstName = $("#ba-firstName").val().trim();
-          const baLastName = $("#ba-lastName").val().trim();
-          const baFullName = baFirstName + baLastName;
-          const baAddress1 = $("#ba-address1").val().trim();
-          if (fullName.length > 30) {
-            document.getElementById("firstName").setCustomValidity('Your combined first/last name cannot be greater than 30 characters.');
-            document.getElementById("lastName").setCustomValidity('Your combined first/last name cannot be greater than 30 characters');
-            $("#firstName-invalid-feedback").text("Your combined first/last name cannot be greater than 30 characters");
-            $("#lastName-invalid-feedback").text("Your combined first/last name cannot be greater than 30 characters");
-          } else {
-            document.getElementById("firstName").setCustomValidity('');
-            document.getElementById("lastName").setCustomValidity('');
-            $("#firstName-invalid-feedback").text("This field is required.");
-            $("#lastName-invalid-feedback").text("This field is required.");
-          }
-          if (baFullName.length > 30) {
-            document.getElementById("ba-firstName").setCustomValidity('Your combined first/last name cannot be greater than 30 characters.');
-            document.getElementById("ba-lastName").setCustomValidity('Your combined first/last name cannot be greater than 30 characters');
-            $("#ba-firstName-invalid-feedback").text("Your combined first/last name cannot be greater than 30 characters");
-            $("#ba-lastName-invalid-feedback").text("Your combined first/last name cannot be greater than 30 characters");
-          } else {
-            document.getElementById("firstName").setCustomValidity('');
-            document.getElementById("lastName").setCustomValidity('');
-            $("#firstName-invalid-feedback").text("This field is required.");
-            $("#lastName-invalid-feedback").text("This field is required.");
-          }
-          if (address1.length > 30) {
-              document.getElementById("address1").setCustomValidity('Address 1 cannot be greater than 30 characters.');
-              $("#address1-invalid-feedback").text("Address 1 cannot be greater than 30 characters");
-          } else {
-              document.getElementById("address1").setCustomValidity('');
-              $("#address1-invalid-feedback").text("This field is required");
-          }
-          if (baAddress1.length > 30) {
-              document.getElementById("ba-address1").setCustomValidity('Address 1 cannot be greater than 30 characters.');
-              $("#ba-address1-invalid-feedback").text("Address 1 cannot be greater than 30 characters");
-          } else {
-              document.getElementById("address1").setCustomValidity('');
-              $("#ba-address1-invalid-feedback").text("This field is required");
-          }
-          if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-          }
-
-          form.classList.add('was-validated');
-
-        }, false);
-      });
-    }, false);
-})();
-
-function setToken() {
-    const now = new Date();
-    const unixTimestamp = now.getTime();
-    const accessToken = sessionStorage.getItem("accessToken");
-    const accessTokenExpireTimestamp = Number(sessionStorage.getItem("accessTokenExpireTimestamp"));
-    const refreshTokenExpireTimestamp = Number(sessionStorage.getItem("refreshTokenExpireTimestamp"));
-    if (!accessToken || (unixTimestamp > refreshTokenExpireTimestamp)) {
-        return newAccessToken();
-    } else if (accessToken && (unixTimestamp < accessTokenExpireTimestamp)) {
-        return accessToken;
-    } else if (accessToken && (unixTimestamp > accessTokenExpireTimestamp) && (unixTimestamp < refreshTokenExpireTimestamp)) {
-        return useRefreshToken();
+    function setToken() {
+        const now = new Date();
+        const unixTimestamp = now.getTime();
+        const accessToken = sessionStorage.getItem("accessToken");
+        const accessTokenExpireTimestamp = Number(sessionStorage.getItem("accessTokenExpireTimestamp"));
+        const refreshTokenExpireTimestamp = Number(sessionStorage.getItem("refreshTokenExpireTimestamp"));
+        if (!accessToken || (unixTimestamp > refreshTokenExpireTimestamp)) {
+            return newAccessToken();
+        } else if (accessToken && (unixTimestamp < accessTokenExpireTimestamp)) {
+            return accessToken;
+        } else if (accessToken && (unixTimestamp > accessTokenExpireTimestamp) && (unixTimestamp < refreshTokenExpireTimestamp)) {
+            return useRefreshToken();
+        }
     }
-}
 
-function newAccessToken() {
-    console.log("newAccessToken()")
-    const now = new Date();
-    const unixTimestamp = now.getTime();
-    $.ajax("/api/access-token", {
-        method: "POST",
-        error: function(error) {
-            console.log(error)
-        }
-    }).then(function(data){
-        const accessToken = data.access_token;
-        const accessTokenExpireTimestamp = unixTimestamp + (data.expires_in * 1000);
-        const refreshToken = data.refresh_token;
-        const refreshTokenExpireTimestamp = unixTimestamp + (data.refresh_expires_in * 1000);
-        const token = {
-            accessToken,
-            accessTokenExpireTimestamp,
-            refreshToken,
-            refreshTokenExpireTimestamp
-        }
-        for (const property in token) {
-            sessionStorage.setItem(property, token[property])
-        }
-        return sessionStorage.getItem("refreshToken");
-    })
-}
-
-function useRefreshToken() {
-    console.log("useRefreshToken()")
-    const now = new Date();
-    const unixTimestamp = now.getTime();
-    let refreshToken = sessionStorage.getItem("refreshToken");
-    $.ajax("/api/refresh-token", {
-        method: "POST",
-        data: "refresh_token=" + refreshToken,
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-        },
-        error: function(error) {
-            console.log(error)
-        }
-    }).then(function(data) {
-        const accessToken = data.access_token;
-        const accessTokenExpireTimestamp = unixTimestamp + (data.expires_in * 1000);
-        refreshToken = data.refresh_token;
-        const refreshTokenExpireTimestamp = unixTimestamp + (data.refresh_expires_in * 1000);
-        const token = {
-            accessToken,
-            accessTokenExpireTimestamp,
-            refreshToken,
-            refreshTokenExpireTimestamp
-        }
-        for (const property in token) {
-            sessionStorage.setItem(property, token[property])
-        }
-        return sessionStorage.getItem("refreshToken");
-    })
-}
-
-$("#getPrintJobs").on("click", function() {
-    console.log("#getPrintJobs works!");
-    const accessToken = setToken();
-    $.ajax("https://api.sandbox.lulu.com/print-jobs/", {
-        method: "GET",
-        headers: {
-            Authorization: "Bearer " + accessToken
-        }, 
-        error: function(error) {
-            console.log(error)
-        }
-    }).then(function(data) {
-        console.log(data);
-    })
-});
-
-$("#quantity").on("change", function() {
-    const quantity = Number($(this).val());
-    let total = quantity * 12;
-    $("#total").html(total);
-})
-
-$("#same-address").on("change", function() {
-    if ($(this).prop("checked")) {
-        $("#billing-address").addClass("d-none")
-    } else {
-        $("#billing-address").removeClass("d-none")
+    function newAccessToken() {
+        console.log("newAccessToken()")
+        const now = new Date();
+        const unixTimestamp = now.getTime();
+        $.ajax("/api/access-token", {
+            method: "POST",
+            error: function (error) {
+                console.log(error)
+            }
+        }).then(function (data) {
+            const accessToken = data.access_token;
+            const accessTokenExpireTimestamp = unixTimestamp + (data.expires_in * 1000);
+            const refreshToken = data.refresh_token;
+            const refreshTokenExpireTimestamp = unixTimestamp + (data.refresh_expires_in * 1000);
+            const token = {
+                accessToken,
+                accessTokenExpireTimestamp,
+                refreshToken,
+                refreshTokenExpireTimestamp
+            }
+            for (const property in token) {
+                sessionStorage.setItem(property, token[property])
+            }
+            return sessionStorage.getItem("refreshToken");
+        })
     }
-})
+
+    function useRefreshToken() {
+        console.log("useRefreshToken()")
+        const now = new Date();
+        const unixTimestamp = now.getTime();
+        let refreshToken = sessionStorage.getItem("refreshToken");
+        $.ajax("/api/refresh-token", {
+            method: "POST",
+            data: "refresh_token=" + refreshToken,
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            error: function (error) {
+                console.log(error)
+            }
+        }).then(function (data) {
+            const accessToken = data.access_token;
+            const accessTokenExpireTimestamp = unixTimestamp + (data.expires_in * 1000);
+            refreshToken = data.refresh_token;
+            const refreshTokenExpireTimestamp = unixTimestamp + (data.refresh_expires_in * 1000);
+            const token = {
+                accessToken,
+                accessTokenExpireTimestamp,
+                refreshToken,
+                refreshTokenExpireTimestamp
+            }
+            for (const property in token) {
+                sessionStorage.setItem(property, token[property])
+            }
+            return sessionStorage.getItem("refreshToken");
+        })
+    }
+
+    $("#getPrintJobs").on("click", function () {
+        console.log("#getPrintJobs works!");
+        const accessToken = setToken();
+        $.ajax("https://api.sandbox.lulu.com/print-jobs/", {
+            method: "GET",
+            headers: {
+                Authorization: "Bearer " + accessToken
+            },
+            error: function (error) {
+                console.log(error)
+            }
+        }).then(function (data) {
+            console.log(data);
+        })
+    });
+
+    // Calculate the total in the shopping cart.
+    $("#quantity").on("change", function () {
+        const quantity = Number($(this).val());
+        let total = quantity * 12;
+        $("#total").html(total);
+    })
+
+    // Validate the shipping address and/or billing address.
+    $("#pay").on("click", function(event) {
+        const forms = $(".needs-validation");
+        forms.each(function(index, form) {
+            $(form).addClass("was-validated");
+        })
+    })
+
+    // Show or Hide the Billing Address at checkout.
+    $("#same-address").on("change", function() {
+        if ($(this).prop("checked")) {
+            $("#billing-address").addClass("d-none");
+            $("#billing-address").removeClass("needs-validation");
+            $("#billing-address").removeClass("was-validated");
+        } else {
+            $("#billing-address").removeClass("d-none");
+            $("#billing-address").addClass("needs-validation");
+        }
+    })
+
+});
